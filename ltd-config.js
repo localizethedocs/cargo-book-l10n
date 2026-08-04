f7dbb13c46e95a28b57cf6de77d614804b0edc7c
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-04 11:37:15 +0000

    Deploying to pages from @ localizethedocs/cargo-book-l10n@9937ba2a2fd35651c05a7c810e06b498dd988f36 🚀

---
+++ ltd-config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["nightly", "Nightly"],
+    ["beta",    "Beta"],
+    ["stable",  "Stable"],
+    ["1",       "Release 1.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/cargo-book-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/cargo-book-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/cargo-book-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/cargo-book-l10n"],
+  ]
+};
